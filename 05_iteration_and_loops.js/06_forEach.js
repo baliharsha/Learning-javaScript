1bbe9fe04a5b722acbf824d048079e0f45a84297
Harsha Bali <149695078+baliharsha@users.noreply.github.com>
2024-02-01 17:58:30 +0000

    loop

---
+++ 05_iteration_and_loops.js/06_forEach.js
@@ -0,0 +1,16 @@
+const coding = ["java", "ruby", "py", "javascript"]
+
+// coding.forEach(function (val) {
+//     console.log(val);
+// })
+
+// ----------OR-------------------
+
+coding.forEach( (item) => {
+    console.log(item);
+})
+output=> java
+         ruby
+         py
+         javascript
+ 
